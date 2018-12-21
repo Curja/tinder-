e478bbaa81ffdc8674a07cfaa9904b62203aabfd
satoru02 <sugiwaka02@gmail.com>
2018-12-21 15:34:21 +0900

    [add]reaciton

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_081601) do
+ActiveRecord::Schema.define(version: 2018_12_21_062807) do
+
+  create_table "reactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
