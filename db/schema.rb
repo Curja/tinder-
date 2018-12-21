77ade5983c7538cc2e9dd4ab127dd1e14c7f643c
satoru02 <sugiwaka02@gmail.com>
2018-12-21 17:03:25 +0900

    [add]matching_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_062807) do
+ActiveRecord::Schema.define(version: 2018_12_21_075117) do
 
   create_table "reactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "to_user_id", null: false
+    t.integer "from_user_id", null: false
+    t.integer "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
