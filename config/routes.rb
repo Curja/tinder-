5cb45f187195e9dddaaeee1769c9f9f5dd587896
satoru02 <sugiwaka02@gmail.com>
2018-12-21 15:42:59 +0900

    [add]create_action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'top#index'
 
   resources :users, only: [:index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reactions,  only: [:create]
 end
