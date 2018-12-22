ab7298b68ac0bab09d9a66b340e1b157576cd095
satoru02 <sugiwaka02@gmail.com>
2018-12-22 19:10:57 +0900

    [add]chat

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -13,5 +13,8 @@ class ChatController < ApplicationController
   end
 
   def show
+    chat_room = ChatRoom.find_by(id: params[:id])
+    @chat_room_user = chat_room.chat_room_users.where.not(user_id: current_user.id).first.user
+    @chat_messages = ChatMessage.where(chat_room: chat_room).order(:created_at)
   end
 end
