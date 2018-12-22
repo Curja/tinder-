5925ba2dd483affc96ecf05dc15cddf422e7ec1c
satoru02 <sugiwaka02@gmail.com>
2018-12-23 00:14:58 +0900

    [fix]chat_controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,12 +1,13 @@
 class ChatController < ApplicationController
+
   def create
-     current_user_char_rooms = ChatRoomUser.where(user_id: current_user.id).map(&:chat_room)
-     chat_room = ChatRoomUser.where(chat_room: current_user_char_rooms, user_id: params[:user_id]).map(&:chat_room).first
-　　　
+     current_user_chat_rooms = ChatRoomUser.where(user_id: current_user.id).map(&:chat_room)
+     chat_room = ChatRoomUser.where(chat_room: current_user_chat_rooms, user_id: params[:user_id]).map(&:chat_room).first
+
      if chat_room.blank?
-      chat_room = ChatRoom.create
-      ChatRoomUser.create(chat_room: chat_room, user_id: current_user.id)
-      ChatRoomuser.create(chat_room: chat_room, user_id: params[:user_id])
+       chat_room = ChatRoom.create
+       ChatRoomUser.create(chat_room: chat_room, user_id: current_user.id)
+       ChatRoomUser.create(chat_room: chat_room, user_id: params[:user_id])
      end
 
      redirect_to action: :show, id: chat_room.id
